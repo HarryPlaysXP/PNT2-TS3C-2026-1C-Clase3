22d8a8a82275b20a1cc4714a1e0694991f21e7fa
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-04-07 21:09:35 -0300

    clase 3 ej3

--- Clase 3/ej3.js
+++ Clase 3/ej3.js
@@ -1,6 +1,7 @@
 console.log ("Guia de ejercicios ES6")
 console.log ("-".repeat(100))
 console.log ("-Ejercicio 3 - Objetos y sus propiedades")
+console.log ("A)")
 const libro = {
     titulo: "El Gran Gatsby",
     autor:"F. Scott Fitzgerald",
@@ -9,3 +10,44 @@ const libro = {
 console.log(libro.autor);
 console.log(libro.titulo);
 console.log(libro.anioPublicacion);
+console.log ("B)")
+const persona = {
+    hobby: "Libros",
+    edad: 30
+};
+console.log(persona.edad);
+persona.edad = 24;
+console.log(persona.edad);
+console.log ("C)")
+const suma = (a , b) => {
+    console.log(a + b);
+}
+const resta = (a , b) => {
+    console.log(a - b);
+}
+const multi = (a , b) => {
+    console.log(a * b);
+}
+const div = (a , b) => {
+    if (b != 0){
+        console.log(a / b);
+    } else {
+        console.log("Al infinito y más alla!");
+    }
+}
+let a = 100
+let b = 10
+let c = 0
+suma(a,b)
+resta(a,b)
+multi(a,b)
+div(a,b)
+div(a,c)
+console.log ("D)")
+const objeto = {
+    propiedad1: "Valor 1",
+    propiedad2: "Valor 2",
+    propiedad3: "Valor 3"
+}
+let propiedad1 = objeto.propiedad1;
+console.log(propiedad1);
