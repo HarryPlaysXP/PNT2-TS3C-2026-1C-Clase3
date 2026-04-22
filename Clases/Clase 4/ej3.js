5185e8166ece3c8cb30750f98249b978033f6cda
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-04-22 19:02:03 -0300

    Clase 4 ej3

--- Clases/Clase 4/ej3.js
+++ Clases/Clase 4/ej3.js
@@ -2,13 +2,18 @@ console.log ("Practica - Clase 4")
 console.log ("-".repeat(100))
 console.log ("Ejercicio 3: Listar Álbumes de Fotos")
 
-fetch("https://jsonplaceholder.typicode.com/albums?userId=1")
-  .then(res => res.json())
-  .then(data => {
-    let personajes = data.results.slice(0, 10);
+const axios = require("axios");
 
-    personajes.forEach(p => {
-      console.log(p.name);
-    });
-  })
-  .catch(error => console.log(error));
+async function listarAlbumes() {
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/albums?userId=1");
+    
+    console.log("Álbumes del usuario 1:");
+    console.log(response.data);
+
+  } catch (error) {
+    console.error("Error al obtener los álbumes:", error);
+  }
+}
+
+listarAlbumes();
