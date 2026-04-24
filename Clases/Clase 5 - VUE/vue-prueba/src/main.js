ec2137976d1b781b7f4c797c7cba91143e88d123
HarryPlaysXP <matiascorrales2002@gmail.com>
2026-04-23 22:39:19 -0300

    agrego bootsrrap y actualizo main importandolo

--- Clases/Clase 5 - VUE/vue-prueba/src/main.js
+++ Clases/Clase 5 - VUE/vue-prueba/src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// agrego importacion vista en clase de de bootstrap y bootstrap icons, ademas de su css para que se apliquen los estilos a los componentes
+Main.js
+import 'bootstrap'
+//import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 createApp(App).mount('#app')
